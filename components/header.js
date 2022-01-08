29a76d6e4d47bd82b299567e1ca456633ec2895a
unknown <dylan.pilsner@gmail.com>
2022-01-08 13:14:26 -0300

    animations agregadas

--- components/header.js
+++ components/header.js
@@ -10,43 +10,34 @@ function headerComponent(el) {
     <div class="header__hamburger-bottom-bread"></div>
   </div>
   <nav class="header__desktop-nav-container">
-    <a href="./portfolio.html" class="header__desktop-nav-item"
-      >Portfolio</a
-    >
-    <a href="./servicios.html" class="header__desktop-nav-item"
-      >Servicios</a
-    >
-    <a href="./contacto.html" class="header__desktop-nav-item"
-      >Contacto</a
-    >
+    <a href="./portfolio.html" class="header__desktop-nav-item">Portfolio</a>
+    <a href="./servicios.html" class="header__desktop-nav-item">Servicios</a>
+    <a href="./contacto.html" class="header__desktop-nav-item">Contacto</a>
   </nav>
   <div class="header__nav-menu">
-    <div class="header__close-nav-menu-cont">
-      <div class="header__close-nav-menu">×</div>
-    </div>
     <div class="header__nav-menu-item-cont">
-      <a href="./portfolio.html" class="header__nav-menu-item"
-        >Portfolio</a
-      >
-      <a href="./servicios.html" class="header__nav-menu-item"
-        >Servicios</a
-      >
-      <a href="./contacto.html" class="header__nav-menu-item"
-        >Contacto</a
-      >
+      <a href="./portfolio.html" class="header__nav-menu-item">Portfolio</a>
+      <a href="./servicios.html" class="header__nav-menu-item">Servicios</a>
+      <a href="./contacto.html" class="header__nav-menu-item">Contacto</a>
     </div>
   </div>
 </header>`;
 
   const openMenu = componentEl.querySelector(".header__open-nav-menu");
+  let closeMenu = componentEl.querySelector(".open");
   const menu = componentEl.querySelector(".header__nav-menu");
   openMenu.addEventListener("click", (e) => {
-    menu.style.display = "initial";
-    openMenu.style.display = "none";
+    if (!closeMenu) {
+      menu.style.display = "initial";
+      openMenu.classList.toggle("open");
+      document.querySelector(".header__nav-menu").classList.toggle("open");
+    }
   });
-  menu.addEventListener("click", (e) => {
-    menu.style.display = "";
-    openMenu.style.display = "";
+
+  openMenu.addEventListener("click", (e) => {
+    if (!componentEl.querySelector(".open")) {
+      menu.style.display = "none";
+    }
   });
 
   el.appendChild(componentEl);
